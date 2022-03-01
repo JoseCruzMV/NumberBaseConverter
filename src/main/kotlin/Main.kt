1d3c0f5add50b85cf7c5b1c12d1fcf56a1f1f428
Jose C Morgado Vargas <josecruzmv@outlook.com>
2022-03-01 12:44:26 -0600

    Done: conversion from decimal to binary, octal and hexadecimal

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,34 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+package converter
 
-    // Try adding program arguments at Run/Debug configuration
-    println("Program arguments: ${args.joinToString()}")
+fun main() = work()
+
+fun work() {
+    decimalConversion()
+}
+
+fun decimalConversion() {
+    print("Enter number in decimal system: ")
+    val decimal = readLine()!!.toIntOrNull() ?: 0
+    print("Enter target base: ")
+    val base = readLine()!!.toIntOrNull() ?: 0
+    val result = decimalToRadix(decimal = decimal, radix = base)
+    println("Conversion result: $result")
+}
+
+fun decimalToRadix(decimal: Int, radix: Int): String {
+    var d = decimal
+    val s = StringBuilder()
+
+    while (d >= radix) {
+        var number: String = (d % radix).toString()
+        s.append(decimalToHex(number = number))
+        d /= radix
+    }
+    s.append( decimalToHex(number = d.toString()) ) // Last remainder
+    return s.toString().reversed()
+}
+
+fun decimalToHex(number: String): String {
+    val hexLetters = mapOf("10" to "A", "11" to "B", "12" to "C", "13" to "D", "14" to "E", "15" to "F")
+    return if (hexLetters.containsKey(number)) hexLetters[number] ?: number else number
 }
